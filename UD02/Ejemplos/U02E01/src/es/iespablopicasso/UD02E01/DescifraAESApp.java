7e296e34c41ac8f58bfde59c68113e248df3e9b5
Manuel Montoro <erlukasgrijander@gmail.com>
2022-09-24 13:27:56 +0200

    Finalizado Ejemplo cifrado simetrico

--- UD02/Ejemplos/U02E01/src/es/iespablopicasso/UD02E01/DescifraAESApp.java
+++ UD02/Ejemplos/U02E01/src/es/iespablopicasso/UD02E01/DescifraAESApp.java
@@ -9,7 +9,7 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public class CifraAESApp {
+public class DescifraAESApp {
   // Valor de salt para generar la clave de 256 bits a partir de la contraseña
   // Este valor es necesario y se puede usar uno aleatorio o fijo. En este caso es fijo
   static String SALT = "Sal y Pimienta";
@@ -19,34 +19,32 @@ public class CifraAESApp {
   static int LONGITUDCLAVE = 256;
 
   public static void main(String[] args) throws Throwable{
-    
     // El mensaje es el primer argumento o el valor por defecto Hola Caracola
-    String mensaje = (args.length > 0) ? args[0] : "Hola Caracola";
+    String mensajeCifrado = (args.length > 0) ? args[0] : "0AHF661MRpNDy1p8dKt64A==";
     // La password es el segundo argumento o la cadena 123456789
     String password = (args.length > 1) ? args[1] : "123456789";
     
     // Obtenemos una especificación de la clave a partir de la password
     Key clave = generaClaveDesdePassword(password);
     
-    String resultado = encripta(clave, mensaje);
+    String resultado = desencripta(clave, mensajeCifrado);
     
     // Y lo imprimimos
-    System.out.println("El mensaje cifrado es " + resultado);
+    System.out.println("El mensaje descifrado es " + resultado);
   }
-
-  private static String encripta(Key clave, String mensaje) throws Throwable {
+  private static String desencripta(Key clave, String mensaje) throws Throwable {
 
     // Iniciamos el algoritmo con la clave
     Cipher algoritmo = Cipher.getInstance("AES");
-    algoritmo.init(Cipher.ENCRYPT_MODE, clave);
+    algoritmo.init(Cipher.DECRYPT_MODE, clave);
     
     // Ciframos
-    byte[] mensajeCifrado = algoritmo.doFinal(mensaje.getBytes());
+    byte[] mensajeCifrado = algoritmo.doFinal(Base64.getDecoder().decode(mensaje));
     
     // El resultado es una secuencia de bytes. Para que tenga un formato legible
     // Lo convertimos a Base 64 que es un formato que codifica secuencias binarias
     // usando caracteres imprimibles.
-    return Base64.getEncoder().encodeToString(mensajeCifrado);
+    return new String(mensajeCifrado);
   }
 
   private static Key generaClaveDesdePassword(String password) throws Throwable {
