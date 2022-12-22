eebb52a32c623cfec536a29e1a53043a83bdc588
Manuel Montoro <erlukasgrijander@gmail.com>
2022-12-22 14:29:19 +0100

    Añadido primer ejemplo de metodo de autenticación

---
+++ UD04/ejemplos/auth-otp/src/hlc/ud04/ejemplos/auth/otp/GeneradorException.java
@@ -0,0 +1,9 @@
+package hlc.ud04.ejemplos.auth.otp;
+
+public class GeneradorException extends RuntimeException {
+
+  public GeneradorException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
