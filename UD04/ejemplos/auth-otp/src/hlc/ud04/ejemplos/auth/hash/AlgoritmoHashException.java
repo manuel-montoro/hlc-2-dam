6d1ed66adf11b37cd08604f37a0b30edabc8e80a
Manuel Montoro <erlukasgrijander@gmail.com>
2023-01-11 12:14:35 +0100

    Eliminado

--- UD04/ejemplos/auth-otp/src/hlc/ud04/ejemplos/auth/hash/AlgoritmoHashException.java
+++
@@ -1,9 +0,0 @@
-package hlc.ud04.ejemplos.auth.hash;
-
-public class AlgoritmoHashException extends RuntimeException {
-
-  public AlgoritmoHashException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
-}
--- UD04/ejemplos/auth-otp/src/hlc/ud04/ejemplos/auth/otp/GeneradorException.java
+++
@@ -1,9 +0,0 @@
-package hlc.ud04.ejemplos.auth.otp;
-
-public class GeneradorException extends RuntimeException {
-
-  public GeneradorException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
-}
